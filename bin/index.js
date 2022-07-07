a2a77c9cd1b92771af11904585be26a2e8df623b
dethMastery <owner@detzz.in.th>
2022-07-08 06:18:55 +0700

    updated: finished all command

--- bin/index.js
+++ bin/index.js
@@ -41,16 +41,59 @@ let opt = require("yargs")
 let name = `${opt.n}`
 let type = `${opt.t}`
 
-console.log('\x1b[32m' ,'yarn Checking~')
+console.log('')
+console.log('\x1b[36m' ,'yarn Checking~')
 console.log('')
 
-exec("yarn --version", (error, stdout, stderr) => {
+exec("yarn --version", (error, stdout) => {
   if(error) {
     console.log('\x1b[31m', 'yarn not found')
-    console.log('\x1b[0m', "please install 'yarn' first")
+    console.log('\x1b[0m', "please install 'yarn' first!")
   }
   
   if(stdout) {
-    console.log('\x1b[36m', 'yarn Ready!')
+    console.log('\x1b[32m', 'yarn Ready!')
+    console.log('')
+    console.log('\x1b[36m' ,'Creating project~')
+    console.log('\x1b[0m', "Tooltips: if it's struck, just 'CTRL+C' or using anything that can used to stop this process")
+    console.log('')
+    
+    exec(`yarn create vite ${name} --template ${type}`, (error, stdout) => {
+      if(error) {
+        console.log('\x1b[31m', error);
+      }
+
+      if(stdout) {
+        console.log('\x1b[32m', 'Project Folder Ready!')
+        console.log('\x1b[0m', "Please wait for me for a while :(")
+        console.log('')
+
+        console.log('\x1b[36m' ,'yarn is now installing your package~')
+        console.log('')
+
+        exec(`cd ${name}`, (error, stdout) => {
+          if(error) {
+            console.log('\x1b[31m', error);
+          }
+
+          exec(`yarn`, (error, stdout) => {
+            if(error) {
+              console.log('\x1b[31m', error);
+            }
+            
+            if(stdout) {
+              console.log(stdout);
+              console.log('\x1b[32m', "Setting up is now complete, now it's your coding time :D")
+              console.log('\x1b[32m', "now just run :")
+              console.log('')
+              console.log('\x1b[32m', `      cd ${name}`)
+              console.log('\x1b[32m', `      yarn dev`)
+              console.log('')
+              console.log('\x1b[32m', "and then init your project by yourself :)")
+            }
+          })
+        })
+      }
+    })
   }
 })
